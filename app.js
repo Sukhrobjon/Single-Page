d91f6878d14e647a76da6de713ef8541dbbeffab
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-10-19 14:51:04 -0700

    added some comments

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // declarations
-
 const express = require('express');
 const app = express();
 const translations = require('./controllers/translations.js');
@@ -10,27 +9,31 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
 
 
-
+// body parser to 
 app.use(bodyParser.urlencoded({ extended: true }));
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'))
 
-// Handlebars
+// seeting up Handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
 
 // connecting the app with data base and save them in 
-// mongoose ODM
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/single-page', {
     useNewUrlParser: true
 });
 
 
+// connecting the routes grabbing from the translations.js file 
 translations(app);
 
+
+// listenning the port
 app.listen(port, () => {
     console.log('App listening on port 3000!')
 })
+
+
 module.exports = app;
